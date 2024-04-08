0a23a89c7c09bc32776a0e2f90fd957fd1bc1fa2
edgar <edgar.dev22@gmail.com>
2024-04-08 08:00:18 -0300

    Exercise Facebook

--- src/count_words.py
+++ src/count_words.py
@@ -1,2 +1,23 @@
+from collections import Counter
+
+
 def count_words(words: list[str], chars: str) -> int:
-    raise NotImplementedError
+    total_length = 0
+    char_count = Counter(chars)
+
+    for word in words:
+        word_count = Counter(word)
+        if all(word_count[char] <= char_count[char] for char in word):
+            total_length += len(word)
+
+    return total_length
+
+
+if __name__ == "__main__":
+    words = ["cat", "bt", "hat", "tree"]
+    chars = "atach"
+    print(count_words(words, chars))
+
+    words = ["hello", "world", "students"]
+    chars = "welldonehoneyr"
+    print(count_words(words, chars))
